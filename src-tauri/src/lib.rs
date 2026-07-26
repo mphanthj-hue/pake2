ae5ac27989ab1cdd7b9497f141663d129a7083ac
mrken <mrken@users.noreply.github.com>
2026-07-26 21:47:55 +0000

    fix: sửa autostart plugin init cho Windows (bỏ MacosAutostartDaemon)

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -172,10 +172,7 @@ pub fn run_app() {
 
     app_builder = app_builder
         .plugin(tauri_plugin_store::Builder::default().build())
-        .plugin(tauri_plugin_autostart::init(
-            tauri_plugin_autostart::MacosAutostartDaemon::default(),
-            None,
-        ))
+        .plugin(tauri_plugin_autostart::init())
         .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
