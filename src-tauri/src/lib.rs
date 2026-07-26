b20e0301f4b07c2abe0a08002fb98938053e6aa7
mrken <mrken@users.noreply.github.com>
2026-07-26 21:54:35 +0000

    fix: dùng đúng API autostart v2.5.1 (MacosLauncher + None args)

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -172,7 +172,10 @@ pub fn run_app() {
 
     app_builder = app_builder
         .plugin(tauri_plugin_store::Builder::default().build())
-        .plugin(tauri_plugin_autostart::init())
+        .plugin(tauri_plugin_autostart::init(
+            tauri_plugin_autostart::MacosLauncher::Launchd,
+            None::<Vec<String>>,
+        ))
         .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
